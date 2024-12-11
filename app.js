1928151a1f10c3ef3dacea198a7b096a92b7850a
francescocifaldi <francesco.cifaldi@gmail.com>
2024-12-11 14:23:50 +0100

    ADD cors permission OK

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const posts = require('./data/posts.js')
 const postsRouter = require('./routers/posts.js')
 const cors = require('cors')
 
-app.use(cors)
+app.use(cors())
 app.use(express.static('public'))
 app.use(express.json())
 
