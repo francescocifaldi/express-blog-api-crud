3fa211ee969515bd9e0f3e8b5858def1f2b607c4
francescocifaldi <francesco.cifaldi@gmail.com>
2024-12-11 14:11:38 +0100

    ADD cors permission

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 console.log('terminale')
 
-const express = require ('express')
+const express = require('express')
 const app = express()
 const port = 3000
 const posts = require('./data/posts.js')
 const postsRouter = require('./routers/posts.js')
+const cors = require('cors')
 
+app.use(cors)
 app.use(express.static('public'))
 app.use(express.json())
 
 app.get('/', (req, res) => {
-	res.send('root')
+    res.send('root')
 })
 
 app.use('/posts', postsRouter)
